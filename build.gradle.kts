a819b44ed31393e61b1d34def6bebc53009a13e6
Brandom Rodriguez <brandomrobor@tuta.io>
2020-08-30 19:00:49 -0600

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+buildscript {
+    repositories {
+        google()
+        jcenter()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:4.0.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.72")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        jcenter()
+    }
+}
+
+tasks {
+    val clean by registering(Delete::class) {
+        delete(buildDir)
+    }
+}
