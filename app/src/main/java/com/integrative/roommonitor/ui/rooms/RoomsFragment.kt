0184c30888eca389aa98582bac23d0b2469aa34c
Brandom Rodriguez <brandomrobor@tuta.io>
2020-10-17 15:19:44 -0600

    Switch to DayNight theme Also solved an issue with the roomDetails observer

--- app/src/main/java/com/integrative/roommonitor/ui/rooms/RoomsFragment.kt
+++ app/src/main/java/com/integrative/roommonitor/ui/rooms/RoomsFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.observe
 import com.integrative.roommonitor.R
 import com.integrative.roommonitor.databinding.FragmentRoomsBinding
 import dagger.hilt.android.AndroidEntryPoint
