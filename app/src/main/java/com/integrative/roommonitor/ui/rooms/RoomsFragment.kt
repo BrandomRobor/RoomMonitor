ec96f6b7d0443917f4d8e96117fd83a6dea4f421
Brandom Rodriguez <brandomrobor@tuta.io>
2020-10-20 14:33:21 -0600

    Added error messages and retry button if data does not load And added a message telling the user that no rooms exists in the server

--- app/src/main/java/com/integrative/roommonitor/ui/rooms/RoomsFragment.kt
+++ app/src/main/java/com/integrative/roommonitor/ui/rooms/RoomsFragment.kt
@@ -2,9 +2,11 @@ package com.integrative.roommonitor.ui.rooms
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.observe
+import androidx.paging.LoadState
 import com.integrative.roommonitor.R
 import com.integrative.roommonitor.databinding.FragmentRoomsBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -27,10 +29,29 @@ class RoomsFragment : Fragment(R.layout.fragment_rooms) {
                 RoomDetailsLoadStateAdapter { adapter.retry() },
                 RoomDetailsLoadStateAdapter { adapter.retry() }
             )
+            roomsButtonRetry.setOnClickListener {
+                adapter.retry()
+            }
         }
 
         viewModel.roomsDetails.observe(viewLifecycleOwner) {
             adapter.submitData(viewLifecycleOwner.lifecycle, it)
+        }
+
+        adapter.addLoadStateListener {
+            binding.apply {
+                roomsProgressBar.isVisible = it.source.refresh is LoadState.Loading
+                roomsRecylerView.isVisible = it.source.refresh is LoadState.NotLoading
+                roomsButtonRetry.isVisible = it.source.refresh is LoadState.Error
+                roomsTextError.isVisible = it.source.refresh is LoadState.Error
+
+                if (it.source.refresh is LoadState.NotLoading && it.append.endOfPaginationReached && adapter.itemCount < 1) {
+                    roomsRecylerView.isVisible = false
+                    roomsZeroRooms.isVisible = true
+                } else {
+                    roomsZeroRooms.isVisible = false
+                }
+            }
         }
     }
 
