7cf97b593da0ad34ab0d1086de689d76733758fd
Brandom Rodriguez <brandomrobor@tuta.io>
2020-11-03 15:55:15 -0700

    Added code to display navigation arguments into the details fragment

--- app/src/main/java/com/integrative/roommonitor/ui/details/DetailsFragment.kt
+++ app/src/main/java/com/integrative/roommonitor/ui/details/DetailsFragment.kt
@@ -3,6 +3,7 @@ package com.integrative.roommonitor.ui.details
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
 import com.integrative.roommonitor.R
 import com.integrative.roommonitor.databinding.FragmentDetailsBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -11,10 +12,19 @@ import dagger.hilt.android.AndroidEntryPoint
 class DetailsFragment : Fragment(R.layout.fragment_details) {
     private var _binding: FragmentDetailsBinding? = null
     private val binding get() = _binding!!
+    private val args by navArgs<DetailsFragmentArgs>()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         _binding = FragmentDetailsBinding.bind(view)
+        val roomDetails = args.roomDetails
+
+        binding.apply {
+            fragmentDetailsInclude.apply {
+                detailsRoomTitle.text = roomDetails.title
+                detailsRoomDescription.text = roomDetails.description
+            }
+        }
     }
 
     override fun onDestroyView() {
