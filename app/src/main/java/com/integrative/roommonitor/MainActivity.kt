07ee89522fba1c25699ace1bf0cc581e43c34c81
Brandom Rodriguez <brandomrobor@tuta.io>
2020-11-03 19:27:46 -0700

    Setup navigation drawer

--- app/src/main/java/com/integrative/roommonitor/MainActivity.kt
+++ app/src/main/java/com/integrative/roommonitor/MainActivity.kt
@@ -7,22 +7,43 @@ import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
+import com.integrative.roommonitor.databinding.ActivityMainBinding
+import com.mikepenz.materialdrawer.model.NavigationDrawerItem
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem
+import com.mikepenz.materialdrawer.model.interfaces.iconRes
+import com.mikepenz.materialdrawer.model.interfaces.nameRes
+import com.mikepenz.materialdrawer.util.ExperimentalNavController
+import com.mikepenz.materialdrawer.util.addItems
+import com.mikepenz.materialdrawer.util.setupWithNavController
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
+    private lateinit var binding: ActivityMainBinding
 
+    @ExperimentalNavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.activityNavDrawer.apply {
+            addItems(
+                NavigationDrawerItem(R.id.roomsFragment, PrimaryDrawerItem().apply {
+                    nameRes = R.string.fragment_rooms_title
+                    iconRes = R.drawable.ic_info
+                })
+            )
+        }
 
         val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+            supportFragmentManager.findFragmentById(binding.navHostFragment.id) as NavHostFragment
         navController = navHostFragment.findNavController()
 
-        val appBarConfig = AppBarConfiguration(navController.graph)
+        val appBarConfig = AppBarConfiguration(navController.graph, binding.activityDrawerLayout)
         setupActionBarWithNavController(navController, appBarConfig)
+        binding.activityNavDrawer.setupWithNavController(navController)
     }
 
     override fun onSupportNavigateUp(): Boolean =
