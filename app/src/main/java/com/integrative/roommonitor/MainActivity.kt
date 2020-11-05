e83ee74231d716f4e3268f9cca48f42491415793
Brandom Rodriguez <brandomrobor@tuta.io>
2020-11-05 15:21:08 -0700

    Added proper way to setup toolbar on start, ignoring last commit Also now back button closes the drawer when open

--- app/src/main/java/com/integrative/roommonitor/MainActivity.kt
+++ app/src/main/java/com/integrative/roommonitor/MainActivity.kt
@@ -6,7 +6,8 @@ import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupWithNavController
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.integrative.roommonitor.databinding.ActivityMainBinding
 import com.mikepenz.iconics.typeface.library.community.material.CommunityMaterial
 import com.mikepenz.materialdrawer.iconics.iconicsIcon
@@ -22,12 +23,14 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
     private lateinit var binding: ActivityMainBinding
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     @ExperimentalNavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setSupportActionBar(binding.activityToolbar)
 
         binding.activityNavDrawer.apply {
             addItems(
@@ -38,14 +41,23 @@ class MainActivity : AppCompatActivity() {
             )
         }
 
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(binding.navHostFragment.id) as NavHostFragment
-        navController = navHostFragment.findNavController()
-
-        val appBarConfig = AppBarConfiguration(navController.graph, binding.activityDrawerLayout)
-        binding.activityToolbar.setupWithNavController(navController, appBarConfig)
+        navController =
+            (supportFragmentManager.findFragmentById(binding.navHostFragment.id) as NavHostFragment).findNavController()
+        appBarConfiguration = AppBarConfiguration(navController.graph, binding.activityDrawerLayout)
+        setupActionBarWithNavController(navController, appBarConfiguration)
         binding.activityNavDrawer.setupWithNavController(navController)
+    }
 
-        setSupportActionBar(binding.activityToolbar)
+    override fun onSupportNavigateUp(): Boolean =
+        navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+
+    override fun onBackPressed() {
+        binding.activityDrawerLayout.apply {
+            if (isOpen) {
+                close()
+            } else {
+                super.onBackPressed()
+            }
+        }
     }
 }
