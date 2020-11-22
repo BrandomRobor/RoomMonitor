4ec5e65e459a3a8710be05832e07530f07bae4ec
Brandom Rodriguez <brandomrobor@tuta.io>
2020-11-22 11:49:45 -0700

    Added explicit dependency version Gets rid of the classpath warning on build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,7 @@ android {
 dependencies {
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.4.10")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.4.10")
     implementation("androidx.core:core-ktx:1.3.2")
     implementation("androidx.appcompat:appcompat:1.2.0")
     implementation("androidx.constraintlayout:constraintlayout:2.0.4")
