70ad65419a4f7237dd34e6a8983b5fa302d3a1a9
Brandom Rodriguez <brandomrobor@tuta.io>
2020-11-25 16:11:34 -0700

    Created error message and retry button on Details page

--- app/src/main/java/com/integrative/roommonitor/ui/details/DetailsFragment.kt
+++ app/src/main/java/com/integrative/roommonitor/ui/details/DetailsFragment.kt
@@ -19,6 +19,8 @@ import kotlinx.coroutines.withContext
 
 @AndroidEntryPoint
 class DetailsFragment : Fragment(R.layout.fragment_details) {
+    private lateinit var adapter: ObjectDataAdapter
+
     private var _binding: FragmentDetailsBinding? = null
     private val binding get() = _binding!!
     private val args by navArgs<DetailsFragmentArgs>()
@@ -27,8 +29,8 @@ class DetailsFragment : Fragment(R.layout.fragment_details) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         _binding = FragmentDetailsBinding.bind(view)
+        adapter = ObjectDataAdapter()
         val roomDetails = args.roomDetails
-        val adapter = ObjectDataAdapter()
 
         binding.apply {
             fragmentDetailsInclude.apply {
@@ -58,19 +60,34 @@ class DetailsFragment : Fragment(R.layout.fragment_details) {
 
             detailsObjectRecyclerView.adapter = adapter
 
-            viewLifecycleOwner.lifecycleScope.launch {
-                adapter.submitList(viewModel.getAllObjectsData(roomDetails.id).objects)
-                detailsZeroObjects.isVisible = adapter.itemCount < 1
-            }
+            fillData(roomDetails.id)
 
             viewModel.liveObjectInfo.observe(viewLifecycleOwner) { newInfo ->
                 viewLifecycleOwner.lifecycleScope.launch {
                     adapter.submitList(mapList(adapter.currentList, newInfo))
                 }
             }
+
+            detailsErrorButton.setOnClickListener {
+                detailsErrorMessage.isVisible = false
+                detailsErrorButton.isVisible = false
+                fillData(roomDetails.id)
+            }
         }
 
         viewModel.requestUpdates(roomDetails.id)
+    }
+
+    private fun fillData(id: String) {
+        viewLifecycleOwner.lifecycleScope.launch {
+            try {
+                adapter.submitList(viewModel.getAllObjectsData(id).objects)
+                binding.detailsZeroObjects.isVisible = adapter.itemCount < 1
+            } catch (e: Exception) {
+                binding.detailsErrorMessage.isVisible = true
+                binding.detailsErrorButton.isVisible = true
+            }
+        }
     }
 
     private suspend fun mapList(list: List<ObjectData>, update: ObjectData): List<ObjectData> =
